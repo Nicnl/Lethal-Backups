12133937e60e10caf2eae04b3d566304c6f8f22d
Lim Nicolas <1387951+Nicnl@users.noreply.github.com>
2024-09-19 04:07:39 +0200

    Embedded the UI into the binary and added build instructions

---
+++ README.txt
@@ -0,0 +1,9 @@
+# How to build:
+
+```
+cd vue
+Remove-Item "dist" -Force -Recurse
+npm run build
+cd ..
+go build -o lethal_backups.exe .
+```
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"io/fs"
 	"lethal_company_save_manager/routes"
 	"lethal_company_save_manager/save_historizer"
+	"log"
+	"net/http"
+	"os/exec"
+	"runtime"
 	"time"
+)
+
+var (
+	//go:embed vue/dist
+	vueApp embed.FS
 )
 
 func main() {
@@ -22,22 +33,66 @@ func main() {
 		}
 	}()
 
-	// 2. Start the web server
-	r := gin.Default()
+	// 2. Start the API web server
+	go func() {
+		r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
-		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	}))
+		r.Use(cors.New(cors.Config{
+			AllowAllOrigins: true,
+			AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		}))
 
-	r.GET("/saves", routes.GetSaves)
-	r.GET("/items", routes.GetItems)
-	r.GET("/item_icon/:itemName", routes.GetItemIcon)
-	r.GET("/planets", routes.GetPlanets)
-	r.POST("/restore/:hash", routes.PostRestoreHash)
+		r.GET("/saves", routes.GetSaves)
+		r.GET("/items", routes.GetItems)
+		r.GET("/item_icon/:itemName", routes.GetItemIcon)
+		r.GET("/planets", routes.GetPlanets)
+		r.POST("/restore/:hash", routes.PostRestoreHash)
 
-	err := r.Run("127.0.0.1:51245")
-	if err != nil {
-		panic(err)
+		err := r.Run("127.0.0.1:51245")
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// 3. Start the Vue.js web server
+	go func() {
+		r := gin.Default()
+
+		vueSubFs, err := fs.Sub(vueApp, "vue/dist")
+		if err != nil {
+			panic(err)
+		}
+		r.StaticFS("/", http.FS(vueSubFs))
+
+		err = r.Run("127.0.0.1:51246")
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	time.Sleep(750 * time.Millisecond)
+	openbrowser("http://127.0.0.1:51246")
+
+	// 4. Wait forever
+	select {}
+}
+
+// https://gist.github.com/hyg/9c4afcd91fe24316cbf0
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
 	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
